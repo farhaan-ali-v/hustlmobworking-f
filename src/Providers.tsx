70e320c7686320851c9285b746a2bd2d7ebf990c
FarhaanAli Vohra <farhaanali.work@gmail.com>
2025-06-29 20:19:41 +0530

    Updated Providers.tsx

--- src/Providers.tsx
+++ src/Providers.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { WalletProvider } from '@txnlab/use-wallet';
 import { PeraWalletConnect } from '@perawallet/connect';
-import BlockchainLoggerProvider from '../components/BlockchainLoggerProvider';
+mport { BlockchainLoggerProvider } from '../components/BlockchainLoggerProvider';
 import StripeProvider from '../components/StripeProvider';
 import { TranslationProvider } from '../components/TranslationProvider';
 import { LingoProviderWrapper, loadDictionary } from 'lingo.dev/react/client';
