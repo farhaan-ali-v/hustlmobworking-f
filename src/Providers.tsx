c320aaa8392cf8304828abdf9f31118cfbad0a9e
FarhaanAli Vohra <farhaanali.work@gmail.com>
2025-06-29 19:51:18 +0530

    Updated Providers.tsx

---
+++ src/Providers.tsx
@@ -0,0 +1,29 @@
+import { WalletProvider } from '@txnlab/use-wallet';
+import { PeraWalletConnect } from '@perawallet/connect';
+import BlockchainLoggerProvider from './components/BlockchainLoggerProvider';
+import StripeProvider from './components/StripeProvider';
+import { TranslationProvider } from './components/TranslationProvider';
+import { LingoProviderWrapper, loadDictionary } from 'lingo.dev/react/client';
+
+const pera = new PeraWalletConnect();
+
+export const Providers = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <WalletProvider
+      providers={[pera]}
+      nodeConfig={{
+        network: 'testnet',
+        nodeServer: 'https://testnet-api.nodely.io',
+        nodeToken: 'BOLTqzcvtetizg512',
+      }}
+    >
+      <BlockchainLoggerProvider>
+        <LingoProviderWrapper loadDictionary={(locale) => loadDictionary(locale)}>
+          <TranslationProvider>
+            <StripeProvider>{children}</StripeProvider>
+          </TranslationProvider>
+        </LingoProviderWrapper>
+      </BlockchainLoggerProvider>
+    </WalletProvider>
+  );
+};
